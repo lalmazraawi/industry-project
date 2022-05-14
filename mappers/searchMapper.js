77651f98cbacb771d41ae9b30e1e102bd4b6a24b
Lailah Amazraawi <lailahamazraawi@Lailahs-MacBook-Pro.local>
2022-05-14 14:17:42 -0400

    comments for filter started

---
+++ mappers/searchMapper.js
@@ -0,0 +1,14 @@
+// make function for restaurant id; 
+//.filter reastaurant id through an object using id # 1-10 
+
+//loop that will filter through array, jump over index, etc 
+
+// if restaurant id == 
+//else ( not selected) move on 
+//return 
+
+
+
+
+
+// make function 2 
