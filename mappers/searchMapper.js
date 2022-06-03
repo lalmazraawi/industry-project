d75da58c53d29587c949e01c31a3591015ef91a6
McKB <mckennakb@comcast.net>
2022-06-02 21:44:26 -0400

    searchMapper ready for testing

--- mappers/searchMapper.js
+++ mappers/searchMapper.js
@@ -1,38 +1,42 @@
-// make function for restaurant id; 
-// .filter reastaurant id through an object using id # 1-10 
-
-// loop that will filter through array, jump over index, etc 
-
-// if restaurant id == 
-// else ( not selected) move on 
-// return 
-
-// make function 2 
-
 const transactions = require('../repository/transactionRepository')
 const restaurants = require('../repository/restaurantRepository')
+const { Op } = require('sequelize')
 
 const filterData = async (req, res) => {
   const {
     restaurantIds, fromDate, toDate, fromHour, toHour, metricCriteria
   } = req.body
 
-  const foundRestaurants = await restaurants.findAll({
-    // where { Id : restaurantIds }
-  })
+  const compareTypeDictionary = {
+    LessThan: Op.lt,
+    LessThanOrEqual: Op.lte,
+    Equal: Op.eq,
+    GreaterThan: Op.gt,
+    GreaterThanOrEqual: Op.gte
+  }
+
+  const searchQuery = {
+    RestaurantId: restaurantIds,
+    BusDt: { [Op.between]: [fromDate, toDate] },
+    OrderTime: { [Op.between]: [(fromHour * 60), (toHour * 60)] }
+  }
+
+  for (let i = 0; i < metricCriteria.length; i++) {
+    const { metricCode, compareType, value } = metricCriteria[i]
+    const seqCompareType = compareTypeDictionary[compareType]
+
+    searchQuery[metricCode] = { [seqCompareType]: value }
+  }
 
   const foundTransactions = await transactions.findAll({
-    // where restaurantIds include RestaurantId
-    // where fromDate - toDate equals BusDt
-    // where fromHour - toHour equals OrderTime
-    // pass metricCriteria to a filterMetrics function
+    where: searchQuery
+  })
+
+  const foundRestaurants = await restaurants.findAll({
+    where: { Id: restaurantIds }
   })
 
   return res.status(200).send({ foundRestaurants, foundTransactions })
-}
-
-const filterMetrics = () => {
-
 }
 
 module.exports = filterData
