c0e24e55e788f4b6032374fb36ebebf91b769cce
Git Class <paultorres1993@gmail.com>
2022-06-08 19:35:06 -0400

    Started to create Models

---
+++ repository/index.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+const restaurants = require('./restaurantRepository')
+const transactions = require('./transactionRepository')
+const metricDefinitions = require('./metricDefinitionRepository')
+
+const environment = process.env.NODE_ENV || 'development'
+const { username, password, database, host, dialect } = config[environment] //eslint-disable-line
+
+const connection = new Sequelize(database, username, password, {
+  host: host,
+  dialect: dialect
+})
+
+const Restaurants = restaurants(connection, Sequelize)
+const Transactions = transactions(connection, Sequelize)
+const MetricDefinitions = metricDefinitions(connection, Sequelize)
+
+Restaurants.hasMany(Transactions)
+Transactions.belongsTo(Restaurants)
+
+
+module.exports = { Restaurants, Transactions, MetricDefinitions, Op: Sequelize.Op }
